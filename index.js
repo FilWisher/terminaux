97831659b365b3829042518dd1074b22ff662b53
FilWisher <williamfisher@hotmail.co.uk>
2015-05-30 21:36:38 +0100

    adds virtualdom

--- index.js
+++ index.js
@@ -1,3 +1,140 @@
+"use strict";
+
+
+var h = require("virtual-dom/h");
+var diff = require("virtual-dom/diff");
+var patch = require("virtual-dom/patch");
+var createElement = require("virtual-dom/create-element");
+var observ = require("observ");
+var observStruct = require("observ-struct");
+var commands = {
+
+    ls: function (args, state) {
+        var current = state.current();
+        var response = Object.keys(state.fs()[current]).join("\n");
+        var history = state.history();
+        history.push(response);
+        state.history.set(history);
+    },
+    cd: function (args, state) {
+        var target = args[1]; 
+        var fs = state.fs();
+        console.log(args);
+        console.log(typeof fs[target])
+        if (typeof fs["/"][target] === "object") {
+            state.current.set(target); 
+            console.log(state.current());
+        }
+    }
+};
+
+function main () {
+    
+    function user () {
+        return "filwisher:~$ ";
+    }
+  
+    var state = observStruct({
+        history: observ([]),
+        current: observ("/"),
+        fs: observStruct({
+            "/": {
+                "documents/": {
+                    "cool": 1,
+                    "yes": 2,
+                    "whynot": 3
+                },
+                "music/": {},
+                "downloads/": {}
+            } 
+        })
+    });
+   
+    var vtree, ntree, initial = true;
+    state(function () {
+        render();   
+    });
+
+    function render () {
+         if (initial) {
+            vtree = view(state); 
+            ntree = createElement(vtree);
+            initial = false;
+            console.log(ntree);
+            return ntree;
+         } else {
+            var newvtree = view(state); 
+            var patches = diff(vtree, newvtree);
+            ntree = patch(ntree, patches);
+            vtree = ntree;
+            document.querySelector("input.cursor").focus()
+         }
+    }
+
+    try {
+    
+        document.querySelector(".terminal-container").appendChild(render());
+    } catch (e) {
+        console.log(e); 
+    }
+
+    function evaluate () {
+        var history = state.history();
+        var input = document.querySelector(".cursor").value;
+        history.push(user() + input);
+        state.history.set(history);
+       
+        var args = input.split(" "); 
+        if (commands.hasOwnProperty(args[0])) {
+        
+            commands[args[0]](args, state);
+        }
+    }
+
+    function view () {
+   
+       return h("div.terminal", [
+     
+            
+            state.history().map(function (command) {
+                return h("div.line", [
+              
+                    h("span", command)
+                ]);
+            }),
+            h("div.line", [
+                h("span", user()),
+                h("input.cursor", {
+                    autofocus: true,
+                    type: "text",
+                    placeholder: "_",
+                    onkeypress: function (ev) {
+                        if (ev.keyCode === 13) {
+                            evaluate();   
+                        } 
+                    }
+                })
+            ])
+       ]); 
+    }
+}
+
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
 var input = document.querySelector("input");
 
 var args;
@@ -35,3 +172,4 @@ var methods = {
     alert("PWD");
   }
 };
+*/
